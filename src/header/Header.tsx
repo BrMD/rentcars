e3178fed587560fccdb68c6d08c097042734bc77
BrMD <dorigon.matheus8@gmail.com>
2023-08-10 12:15:18 -0300

    begginig project structure

---
+++ src/header/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div>Local</div>
+      <div>Logo</div>
+      <div>User</div>
+    </div>
+  );
+};
+
+export default Header;
