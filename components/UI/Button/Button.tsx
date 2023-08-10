89b8ef567549955448aebe30023dea6f69321e69
BrMD <dorigon.matheus8@gmail.com>
2023-08-10 16:37:33 -0300

    styling button

---
+++ components/UI/Button/Button.tsx
@@ -0,0 +1,7 @@
+import "./Button.css";
+
+const Button = ({ children }: { children: React.ReactNode }) => {
+  return <button className="button">{children}</button>;
+};
+
+export default Button;
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,8 +1,14 @@
 import "./Header.css";
+import locationIcon from "/locationIconRed.svg";
+
+import Button from "../../components/UI/Button/Button";
 const Header = () => {
   return (
     <div className="header">
-      <div>Local</div>
+      <div>
+        <img src={locationIcon} width={"20px"} height={"20px"} />
+        <Button>localização</Button>
+      </div>
       <div>Logo</div>
       <div>User</div>
     </div>
