1a62f5c004d4af9bbc68011dcfca200d16a1c20c
BrMD <dorigon.matheus8@gmail.com>
2023-08-17 16:15:55 -0300

    added most rented

--- src/homeContent/HomeContent.tsx
+++ src/homeContent/HomeContent.tsx
@@ -2,30 +2,63 @@ import "./HomeContent.css";
 
 const HomeContent = () => {
   return (
-    <div className="homeShowCategoryCars">
-      <div className="itemCategoryCar">
-        <span>Sedan</span>
-        <img src="sedanSF.png" className="imgCar" />
+    <div>
+      <div className="homeShowCategoryCars">
+        <div className="itemCategoryCar">
+          <span>Sedan</span>
+          <img src="sedanSF.png" className="imgCar" />
+        </div>
+        <div className="itemCategoryCar">
+          <span>Hatch</span>
+          <img src="hatchSF.png" className="imgCar" />
+        </div>
+        <div className="itemCategoryCar">
+          <span>SUV</span>
+          <img src="SUVSF.png" className="imgCar" />
+        </div>
+        <div className="itemCategoryCar">
+          <span>Picape</span>
+          <img src="pickupSF.png" className="imgCar" />
+        </div>
+        <div className="itemCategoryCar">
+          <span>Eletrico</span>
+          <img src="eletricoSF.png" className="imgCar" />
+        </div>
+        <div className="itemCategoryCar">
+          <span>Luxo</span>
+          <img src="luxoSF.png" className="imgCar" />
+        </div>
       </div>
-      <div className="itemCategoryCar">
-        <span>Hatch</span>
-        <img src="hatchSF.png" className="imgCar" />
-      </div>
-      <div className="itemCategoryCar">
-        <span>SUV</span>
-        <img src="SUVSF.png" className="imgCar" />
-      </div>
-      <div className="itemCategoryCar">
-        <span>Picape</span>
-        <img src="pickupSF.png" className="imgCar" />
-      </div>
-      <div className="itemCategoryCar">
-        <span>Eletrico</span>
-        <img src="eletricoSF.png" className="imgCar" />
-      </div>
-      <div className="itemCategoryCar">
-        <span>Luxo</span>
-        <img src="luxoSF.png" className="imgCar" />
+      <div className="moreRentedDiv">
+        <div className="moreRented">
+          <h1 className="title">Mais Alugados</h1>
+        </div>
+        <div className="itemsRented">
+          <div className="rentedItem">
+            <span>Sedan</span>
+            <img src="eletricoSF.png" className="imgCar" />
+          </div>
+          <div className="rentedItem">
+            <span>Sedan</span>
+            <img src="eletricoSF.png" className="imgCar" />
+          </div>
+          <div className="rentedItem">
+            <span>Sedan</span>
+            <img src="eletricoSF.png" className="imgCar" />
+          </div>
+          <div className="rentedItem">
+            <span>Sedan</span>
+            <img src="eletricoSF.png" className="imgCar" />
+          </div>
+          <div className="rentedItem">
+            <span>Sedan</span>
+            <img src="eletricoSF.png" className="imgCar" />
+          </div>
+          <div className="rentedItem">
+            <span>Sedan</span>
+            <img src="eletricoSF.png" className="imgCar" />
+          </div>
+        </div>
       </div>
     </div>
   );
