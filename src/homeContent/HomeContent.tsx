d5f0063758a90bb50bc19461201fa6f147467a8f
BrMD <dorigon.matheus8@gmail.com>
2023-08-17 11:19:55 -0300

    added image

--- src/homeContent/HomeContent.tsx
+++ src/homeContent/HomeContent.tsx
@@ -5,21 +5,27 @@ const HomeContent = () => {
     <div className="homeShowCategoryCars">
       <div className="itemCategoryCar">
         <span>Sedan</span>
+        <img src="sedanSF.png" className="imgCar" />
       </div>
       <div className="itemCategoryCar">
         <span>Hatch</span>
+        <img src="hatchSF.png" className="imgCar" />
       </div>
       <div className="itemCategoryCar">
         <span>SUV</span>
+        <img src="SUVSF.png" className="imgCar" />
       </div>
       <div className="itemCategoryCar">
         <span>Picape</span>
+        <img src="pickupSF.png" className="imgCar" />
       </div>
       <div className="itemCategoryCar">
         <span>Eletrico</span>
+        <img src="eletricoSF.png" className="imgCar" />
       </div>
       <div className="itemCategoryCar">
         <span>Luxo</span>
+        <img src="luxoSF.png" className="imgCar" />
       </div>
     </div>
   );
